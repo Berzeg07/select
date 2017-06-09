e09d54828f93acd01763282581ef442948925426
Berzeg07 <berezg86@gmail.com>
2017-06-09 11:46:14 +0400

    select

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/selectric.css">
+
+	
+</head>
+<body>
+
+	<div class="container">
+		<select>
+			<option value="1">Цене</option>
+			<option value="2">Алфавиту</option>			
+		</select>
+	</div>
+
+
+
+
+
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="js/common.js"></script>
+	<script src="js/jquery.selectric.min.js"></script>
+
+
+</body>
+</html>
